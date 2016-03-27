dc6c2144de667b3c4781eaec0e0152a5d3e8f618
ddodnikov <ddodnikov@gmail.com>
2016-03-27 16:51:58 +0300

    uploadsong

--- src/com/soundcloud/servlets/Upload.java
+++ src/com/soundcloud/servlets/Upload.java
@@ -1,5 +1,5 @@
 package com.soundcloud.servlets;
- 
+
 import java.io.File;
 import java.io.IOException;
 
@@ -10,60 +10,61 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
- 
-@WebServlet("/Upload")
-@MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
-                 maxFileSize=1024*1024*10,      // 10MB
-                 maxRequestSize=1024*1024*50)   // 50MB
-public class Upload extends HttpServlet {
- 
-    /**
-	 * 
-	 */
+
+import com.soundcloud.model.TrackDAO;
+
+@WebServlet("/UploadSong")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 0, // 0MB , always on hard
+														// disk
+		maxFileSize = 1024 * 1024 * 50, // 50MB
+		maxRequestSize = 1024 * 1024 * 500) // 500MB
+public class UploadSong extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
+
+	private static final String SAVE_DIR = "D:\\soundcloudFiles\\tracks";
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// creates the save directory if it does not exists
+		File fileSaveDir = new File(SAVE_DIR);
+		if (!fileSaveDir.exists()) {
+			fileSaveDir.mkdir();
+		}
+
+		boolean hasFile = true;
+		
+		Part part = request.getPart("fileUpload");
+		String fileName = extractFileName(part);
+
+		if (!fileName.equals("") && part.getSize() < 50*1024*1024) {
+			part.write(SAVE_DIR + File.separator + fileName);
+		} else {
+			hasFile = false;
+		}
+		
+		new TrackDAO().addTrack(fileName, "111", "222", SAVE_DIR + "\\" + fileName, 6);
+
+		if (hasFile)
+			request.setAttribute("message", "Upload has been done successfully!");
+		else 
+			request.setAttribute("message", "No file selected or file exceeds maximum size limit of 50MB!");
+
+		getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+	}
+
 	/**
-     * Name of the directory where uploaded files will be saved, relative to
-     * the web application directory.
-     */
-    private static final String SAVE_DIR = "D:\\soundcloudFiles\\tracks";
-     
-    /**
-     * handles file upload
-     */
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        // gets absolute path of the web application
-        String appPath = request.getServletContext().getRealPath("");
-        // constructs path of the directory to save uploaded file
-        String savePath = SAVE_DIR;
-         
-        // creates the save directory if it does not exists
-        File fileSaveDir = new File(savePath);
-        if (!fileSaveDir.exists()) {
-            fileSaveDir.mkdir();
-        }
-         
-        for (Part part : request.getParts()) {
-            String fileName = extractFileName(part);
-            part.write(savePath + File.separator + fileName);
-        }
- 
-        request.setAttribute("message", "Upload has been done successfully!");
-        getServletContext().getRequestDispatcher("/home.jsp").forward(
-                request, response);
-    }
- 
-    /**
-     * Extracts file name from HTTP header content-disposition
-     */
-    private String extractFileName(Part part) {
-        String contentDisp = part.getHeader("content-disposition");
-        String[] items = contentDisp.split(";");
-        for (String s : items) {
-            if (s.trim().startsWith("filename")) {
-                return s.substring(s.indexOf("=") + 2, s.length()-1);
-            }
-        }
-        return "";
-    }
+	 * Extracts file name from HTTP header content-disposition
+	 */
+	private String extractFileName(Part part) {
+		String contentDisp = part.getHeader("content-disposition");
+		String[] items = contentDisp.split(";");
+		for (String s : items) {
+			if (s.trim().startsWith("filename")) {
+				return s.substring(s.indexOf("=") + 2, s.length() - 1);
+			}
+		}
+		return "";
+	}
 }
